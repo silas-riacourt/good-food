0300e666f99eb6fb02c1d868374a2ee7be899d29
Silass_ <silasdu22@gmail.com>
2022-06-13 12:44:12 +0200

    fix: nuxt config

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -2,8 +2,8 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
-
+  target: 'server',
+  ssr: false,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - goodfood',
