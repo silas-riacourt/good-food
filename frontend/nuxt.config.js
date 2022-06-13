99e35450d0565a578ee24c31ffde987ce9400b7c
Silass_ <silasdu22@gmail.com>
2022-06-13 12:49:32 +0200

    fix: nuxt config

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -52,7 +52,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:8080'
+    baseURL: process.env.BASE_URL || 'http://localhost:8080'
   },
   router: {
     middleware: ['auth']
